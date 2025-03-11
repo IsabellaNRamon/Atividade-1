afbbcd464320003d2f4b46340fea3a92ffeed6f9
IsabellaNRamon <isabellanunesramon440@gmail.com>
2025-03-11 09:23:55 -0300

    atividade 1103

--- TesteProdutos.java
+++ TesteProdutos.java
@@ -1,9 +1,9 @@
 public class TesteProdutos {
     public static void main(String[] args) {
         // Criando dois produtos
-        Produto p1 = new Produto("Colher", 3, 5.99, 'U', true);
-        Produto p2 = new Produto("Refrigerente", 5, 7.50, 'B', true);
-        Produto p3 = new Produto( "Pão de milho", 9, 1.50, 'P', false);
+        Produto p1 = new Produto("Concha", 3, 5.99, 'U', true);
+        Produto p2 = new Produto("Suco de laranja", 5, 7.50, 'B', true);
+        Produto p3 = new Produto( "Pão frances", 9, 1.50, 'P', false);
 
         // Imprimindo detalhes dos produtos
         System.out.println("Detalhes do Produto 1:");
